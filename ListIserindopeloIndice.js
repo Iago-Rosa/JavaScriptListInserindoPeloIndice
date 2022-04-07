4bb44a3686903c3d7e6c265a97a527c921f2e287
Iago-Rosa <iagorosa045@gmail.com>
2022-04-07 18:09:41 -0300

    Arquivo

---
+++ ListIserindopeloIndice.js
@@ -0,0 +1,17 @@
+
+//subtituindo valor da lista pelo indice e lendo
+var frutas = ["Pessego","Manga","Maça","limão"];
+
+//inserindo dados atraves do indice.
+frutas[5]="Danone"
+frutas[4]="Abacaxi"
+frutas[0]="kiwi"
+
+
+document.write(frutas)
+console.log(frutas)
+
+
+
+
+
